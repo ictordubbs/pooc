f13d3989333d0088baaa680c715e7255e0d9c2d0
blochsimon22 <blochsimon22@gmail.com>
2018-10-11 12:41:21 +0000

    Ajout TP2

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express')
 const app = express ()
 const port = 3000
 
-app.get('/', (request, response) => {response.send('Requête reçue...')})
 
-app.get('/hello/:name', (request, response) => {
-    response.send('Hello, '+request.params.name+'!');
-})
+const os = require("os");
+const mustacheExpress = require('mustache-express');
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'))
+
+
+app.get('/', (request, response) => {response.render('index');})
+
+
+app.get('/hello/:name', (request, response) => {response.render('hello', {name: request.params.name});})
+
+app.get('/pooc/', (request, response) => {response.render('pooc');})
   
 app.listen(port, (err) => {
     if (err) {
       return console.log('Erreur du serveur : ', err)
     }
-    console.log('Le serveur écoute sur le port' + port + 'nRendez vous sur http://localhost:' + port
-  )})
+    console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);})
 
